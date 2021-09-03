2038910ed1f6d1fc3b3f4ed1e70efcf4763cdccc
Mariana <marianabicla@gmail.com>
2021-09-03 16:31:47 -0300

    iniciando projeto de calculo de area da casa

---
+++ src/areaCasa/AreaCasa.java
@@ -0,0 +1,39 @@
+package areaCasa;
+
+import java.util.Scanner;
+
+public class AreaCasa {
+
+	public static void main(String[] args) {
+		
+		Scanner leitor = new Scanner(System.in);
+		
+		double larguraComodo, comprimentoComodo, areaComodo, areaCasa;
+		
+		String resposta, nomeComodo;
+		
+		System.out.println("---------------------------");
+		System.out.println("Calculo de área de uma casa");
+		System.out.println("---------------------------");
+		System.out.println();
+		
+		System.out.print("Qual o nome do cômodo? ");
+		nomeComodo = leitor.next();
+		
+		System.out.print("Qual a largura? ");
+		larguraComodo = leitor.nextDouble();
+		
+		System.out.print("Qual o comprimento? ");
+		comprimentoComodo = leitor.nextDouble();
+		
+		areaComodo = larguraComodo * comprimentoComodo;
+		
+		System.out.println();
+		
+		System.out.println("O cômodo " + nomeComodo + " tem " + areaComodo + " metros");
+		
+		System.out.print("Você deseja calcular mais cômodos (S/N)? ");
+		resposta = leitor.next();
+	}
+
+}
